7a4101a6aa578ed09104912fa36c215135c07df6
IGubarenko <IGubarenko@IGUBARENKO>
2020-10-12 00:54:44 +0300

    add inner page and pop up

--- Cargo-Dynamic/app/js/main.js
+++ Cargo-Dynamic/app/js/main.js
@@ -1,15 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () { 
-    openBurgerMenu();
-});
+var body = document.body;
 
+document.addEventListener("DOMContentLoaded", function(){
+
+    openBurgerMenu();
+
+    openPopUp();
+
+});
 
 function openBurgerMenu() {
     let burgerNav = document.querySelector(".nav-opener");
-    let nav = document.querySelector(".nav-list");
-    let body = document.body;
+    let nav = document.querySelector(".nav-list");    
 
     burgerNav.addEventListener('click', function () {
         nav.classList.toggle('nav-active'); 
         body.classList.toggle('scroll-hidden');       
     });
 }
+
+function openPopUp() {  
+
+    let openModal = document.querySelector('#openModal');
+    let btnPhone = document.querySelector('.btn-phone');
+    let btnClose = document.querySelector('.btn-close');
+
+    btnPhone.addEventListener('click', function() { 
+        openModal.classList.remove('d-none');        
+        openModal.classList.add('d-block');                      
+    });
+
+    btnClose.addEventListener('click', function() {  
+        openModal.classList.remove('d-block');      
+        openModal.classList.add('d-none');
+    });
+}
+
+
