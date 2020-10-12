6d603c1363298a24c02259d5040382209ac66aa4
IGubarenko <IGubarenko@IGUBARENKO>
2020-10-12 22:51:45 +0300

    change css

--- Cargo-Dynamic/app/js/main.js
+++ Cargo-Dynamic/app/js/main.js
@@ -3,7 +3,6 @@ var body = document.body;
 document.addEventListener("DOMContentLoaded", function(){
 
     openBurgerMenu();
-
     openPopUp();
 
 });
@@ -21,18 +20,18 @@ function openBurgerMenu() {
 function openPopUp() {  
 
     let openModal = document.querySelector('#openModal');
+    let bodyBg = document.querySelector('.body-background');
     let btnPhone = document.querySelector('.btn-phone');
     let btnClose = document.querySelector('.btn-close');
 
-    btnPhone.addEventListener('click', function() { 
-        openModal.classList.remove('d-none');        
-        openModal.classList.add('d-block');                      
+    btnPhone.addEventListener('click', function() {              
+        openModal.classList.remove('off-modal');
+        bodyBg.classList.remove('off-modal');                           
     });
 
     btnClose.addEventListener('click', function() {  
-        openModal.classList.remove('d-block');      
-        openModal.classList.add('d-none');
+        openModal.classList.add('off-modal');
+        bodyBg.classList.add('off-modal');         
     });
 }
-
 
